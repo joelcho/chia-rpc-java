5ec376ccac43da89e2f021d78244a3ed1f5a2640
xuehongyang <1379149253@qq.com>
2021-09-06 16:41:53 +0800

    com.github.joelcho.chia.types.node.SpendBundle类增加coinSpends字段，用于兼容1.2.4版本

--- src/main/java/com/github/joelcho/chia/types/node/SpendBundle.java
+++ src/main/java/com/github/joelcho/chia/types/node/SpendBundle.java
@@ -15,5 +15,24 @@ import java.util.List;
 @Data
 public class SpendBundle {
     private List<CoinSolution> coinSolutions;
+    private List<CoinSolution> coinSpends;
     private G2Element aggregatedSignature;
+
+    /**
+     * 适配1.2.4版本（大约）之前
+     * @param coinSolutions
+     */
+    public void setCoinSolutions(List<CoinSolution> coinSolutions) {
+        this.coinSolutions = coinSolutions;
+        this.coinSpends = coinSolutions;
+    }
+
+    /**
+     * 适配1.2.4版本（大约）之后且包含1.2.4版本
+     * @param coinSpends
+     */
+    public void setCoinSpends(List<CoinSolution> coinSpends) {
+        this.coinSpends = coinSpends;
+        this.coinSolutions = coinSpends;
+    }
 }
